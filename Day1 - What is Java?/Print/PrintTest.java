e855aaf1f672d4250e55c77a32245c43b2a1311d
GyuJin Lee <98343991+JinLeeGG@users.noreply.github.com>
2025-06-24 01:43:51 +0900

    class code

---
+++ Day1 - What is Java?/Print/PrintTest.java
@@ -0,0 +1,11 @@
+package print;
+
+public class PrintTest {
+	public static void main(String[] args) {
+		System.out.println("Hello World!😁");
+		// print name
+		System.out.print("Gyujin Lee");
+		// syso + control + spacebar
+		System.out.println(20);
+	}
+}
